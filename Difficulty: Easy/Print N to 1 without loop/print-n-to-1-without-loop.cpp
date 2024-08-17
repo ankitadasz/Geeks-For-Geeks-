23bfd74dc2d08e90f92e7ab647a7ccaf1a0be001
Ankita Das <159277230+ankitadasz@users.noreply.github.com>
2024-08-17 22:19:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printNos(int N) {
+        // code here
+        int i;
+        for(i=N;i>0 ; i--){
+            cout << i << " ";
+        }
+    }
+    int main(){
+        int N;
+        cin >> N;
+        printNos(N);
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main() {
+    int T;
+
+    // taking testcases
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        // input N
+        cin >> N;
+        Solution ob;
+        // calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
