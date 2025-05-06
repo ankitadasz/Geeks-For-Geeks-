2b9d3522630120b92f0faf4a9406ac7df0ebfe12
Ankita Das <159277230+ankitadasz@users.noreply.github.com>
2025-05-06 22:10:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Pattern 8/pattern-8.cpp
+++ Difficulty: Basic/Pattern 8/pattern-8.cpp
@@ -4,31 +4,25 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
-public:
-	
-	void printTriangle(int n) {
-	    // code here
-	    for (int i = n; i > 0; i--) {
-        
-        for (int j = 0; j < n - i; j++) {
-            cout << " ";
-        }
-        
-        for (int k = 0; k < 2 * i - 1; k++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
 
-    
-	}
-	int main(){
-	    int n;
-	    cin >> n;
-	    printTriangle(n);
-	}
+class Solution {
+  public:
+
+    void printTriangle(int n) {
+        // code here
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i-1;j++){
+                cout<<" ";
+            }
+            for(j=1;j<=2*(n-i)+1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
 };
+
 
 //{ Driver Code Starts.
 
@@ -41,7 +35,9 @@ int main() {
         
         Solution ob;
         ob.printTriangle(n);
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 // } Driver Code Ends
